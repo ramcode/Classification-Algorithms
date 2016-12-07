080d981d00cee5dc5f8401aa3811f307a528404a
nitish <goelnitish@gmail.com>
2016-12-07 17:37:14 -0500

    boilerplate code ready for KNN

--- src/KNearestNeighbour.java
+++ src/KNearestNeighbour.java
@@ -1,7 +1,112 @@
-/**
- * Created by VenkataRamesh on 12/7/2016.
- */
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class KNearestNeighbour {
+
+    private int numOfFolds;
+    private String fileName;
+    public double[][] dataMatrix;
+    private int dataSampleCount;
+
+
+    public KNearestNeighbour ( int numOfFolds, String fileName ) {
+
+        this.numOfFolds = numOfFolds;
+        this.fileName = fileName;
+
+    }
+
+
+    public double[][] readFeatureValues ( String path ) {
+
+        Path filePath = null;
+
+        try {
+
+            filePath = Paths.get(path, fileName);
+            List<String> dataSamples = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+            int rows = dataSamples.size();
+            this.dataSampleCount = rows;
+            int columns = dataSamples.get(0).trim().split("\\s+").length;
+            dataMatrix = new double[rows][columns];
+
+            for ( int i = 0; i < rows; i++ ) {
+
+                String[] singleDataSampleValue = dataSamples.get(i).trim().split("\\s+");
+
+                for ( int j = 0; j < columns; j++ ) {
+
+                    //TODO: give decent numerical values to string data
+                    if ( singleDataSampleValue[j].equals("Absent") ) {
+
+                        dataMatrix[i][j] = 0.00;
+
+                    } else if ( singleDataSampleValue[j].equals("Present") ) {
+
+                        dataMatrix[i][j] = 1.00;
+
+                    } else {
+
+                        dataMatrix[i][j] = Double.parseDouble(singleDataSampleValue[j]);
+
+                    }
+
+                }
+
+            }
+
+
+        } catch ( Exception e ) {
+
+            e.printStackTrace();
+
+        }
+
+        return dataMatrix;
+    }
+
+    public double[][] calculateDistanceMatrix () {
+
+        double[][] distanceMatrix = null;
+
+        try {
+
+            distanceMatrix = new double[dataMatrix.length][dataMatrix.length];
+
+            for ( int i = 0; i < dataMatrix.length - 1; i++ ) {
+
+                for ( int j = i + 1; j < dataMatrix.length; j++ ) {
+
+                    double[] object1 = dataMatrix[i];
+                    double[] object2 = dataMatrix[j];
+                    double squaredSum = 0;
+
+                    for ( int dim = 0; dim < object1.length - 1; dim++ ) {
+
+                        squaredSum += Math.pow(object1[dim] - object2[dim], 2);
+
+                    }
+
+                    double eucDistance = Math.sqrt(squaredSum);
+                    distanceMatrix[i][j] = eucDistance;
+                    distanceMatrix[j][i] = eucDistance;
+
+                }
+
+            }
+
+        } catch ( Exception e ) {
+
+            e.printStackTrace();
+
+        }
+
+        return distanceMatrix;
+
+    }
 
 
 }
--- src/RunKNearestNeighbour.java
+++ src/RunKNearestNeighbour.java
@@ -1,5 +1,36 @@
-/**
- * Created by VenkataRamesh on 12/7/2016.
- */
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class RunKNearestNeighbour {
+
+    public static void main ( String args[] ) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter File name of data set: ");
+        String fileName = sc.nextLine();
+
+        if (fileName == null || fileName.length() == 0) {
+
+            fileName = "project3_dataset1.txt";
+
+        }
+
+        String path = "data/";
+        System.out.println("Enter Cross Validation Fold Number: ");
+        int foldNumber = Integer.valueOf(sc.nextLine());
+        KNearestNeighbour KNN = new KNearestNeighbour(foldNumber, fileName);
+
+        double[][] matrix = KNN.readFeatureValues(path);
+        //double[][] distanceMatrix = KNN.calculateDistanceMatrix();
+
+        /*Arrays.stream(matrix).forEach(x -> {
+            System.out.println(Arrays.toString(x));
+        });*/
+
+/*        Arrays.stream(distanceMatrix).forEach(x -> {
+            System.out.println(Arrays.toString(x));
+        });*/
+
+
+    }
 }
