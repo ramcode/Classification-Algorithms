21f934eb9a240c057bf6a708d432c84a13c87127
DKRR <dkrohith93@gmail.com>
2016-12-10 00:26:24 -0500

    demo KNN part implemented

--- src/RunKNearestNeighbour.java
+++ src/RunKNearestNeighbour.java
@@ -6,24 +6,60 @@ public class RunKNearestNeighbour {
     public static void main ( String args[] ) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter File name of data set: ");
-        String fileName = sc.nextLine();
+        System.out.println("Enter 1 for crossvalidation and 2 for demo");
+        int checker = Integer.valueOf(sc.nextLine());
+        if(checker==1)
+        {
+            System.out.println("Enter File name of data set: ");
+            String fileName = sc.nextLine();
 
-        if (fileName == null || fileName.length() == 0) {
+            if (fileName == null || fileName.length() == 0) {
 
-            fileName = "project3_dataset1.txt";
-            //fileName = "Test.txt";
+                fileName = "project3_dataset1.txt";
+                //fileName = "Test.txt";
 
+            }
+
+            String path = "data/";
+            System.out.println("Enter Cross Validation Fold Number: ");
+            int foldNumber = Integer.valueOf(sc.nextLine());
+            KNearestNeighbour KNN = new KNearestNeighbour(foldNumber, fileName);
+
+            double[][] matrix = KNN.readFeatureValues(path, fileName);
+
+            double[][] distanceMatrix = KNN.calculateDistanceMatrix();
+
+            KNN.startCrossValidation(matrix);
+        }
+        else
+        {
+            System.out.println("Enter Training File name of data set: ");
+            String trainingFileName = sc.nextLine();
+
+            if (trainingFileName == null || trainingFileName.length() == 0) {
+
+                trainingFileName = "project3_dataset3_train.txt";
+
+            }
+
+            System.out.println("Enter Testing File name of data set: ");
+            String testingFileName = sc.nextLine();
+
+            if (testingFileName == null || testingFileName.length() == 0) {
+
+                testingFileName = "project3_dataset3_test.txt";
+
+            }
+
+            String path = "data/";
+            KNearestNeighbour KNN = new KNearestNeighbour(trainingFileName, testingFileName);
+
+            double[][] trainingmatrix = KNN.readFeatureValues(path, trainingFileName);
+            double[][] testingmatrix = KNN.readFeatureValues(path, testingFileName);
+
+            KNN.demoKNNalgorithm(trainingmatrix, testingmatrix);
         }
 
-        String path = "Classification-Algorithms/data/";
-        System.out.println("Enter Cross Validation Fold Number: ");
-        int foldNumber = Integer.valueOf(sc.nextLine());
-        KNearestNeighbour KNN = new KNearestNeighbour(foldNumber, fileName);
-
-        double[][] matrix = KNN.readFeatureValues(path);
-
-        double[][] distanceMatrix = KNN.calculateDistanceMatrix();
 
         /*System.out.println("DataMatrix");
         Arrays.stream(matrix).forEach(x -> {
@@ -36,7 +72,6 @@ public class RunKNearestNeighbour {
             System.out.println(Arrays.toString(x));
         });*/
 
-        KNN.startCrossValidation(matrix);
         //double[][] distanceMatrix = KNN.calculateDistanceMatrix();
 
 
